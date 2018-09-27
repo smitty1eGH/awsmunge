13ae42f5a052b645134f7a12eae1b0b7eaac47cd
Smith <584049@bah.com>
2018-09-27 07:26:58 -0400

    Working on the descent of the results.

--- awsmunge.py
+++ awsmunge.py
@@ -1,9 +1,10 @@
 import ast
+from typing import Dict, List, Union
 
-def awsret2dict(aws_return):
+def awsret2dict(aws_return: str) -> Dict:
     '''Convert the raw AWS_RETURN value for an API call to a python dict.
     '''
-    def wrap_value(frag):
+    def wrap_value(frag: str) -> str:
         '''Pipes, and datetime() calls, in FRAG must be double quoted.
            Be mindful of commas at the end of the line.
         '''
@@ -31,7 +32,7 @@ def awsret2dict(aws_return):
     #https://docs.python.org/3/library/ast.html#ast-helpers
     return ast.literal_eval(''.join(z))
 
-def flatten_awsret(aws_return):
+def flatten_awsret(aws_return: str) -> Dict:
     '''Take the AWS_RETURN, convert to dict via awsret2dict(), then
          do depth-first traversals of that dict, and store the results
          in a new, flattened traversal form (FTF).
@@ -50,19 +51,36 @@ def flatten_awsret(aws_return):
              i=integer
              d=datetime
     '''
-    def type2delim(v_type):
+    def type2delim(v_type: str) -> str:
         '''Return character based upon V_TYPE
         '''
-        #print('v_type %s' % v_type)
-        type_dict={"<class 'list'>"    :','
-                  ,"<class 'str'>"     :'s'
-                  ,'datetime':'d'
-                  ,'int'     :'i'
-                  ,'dict'    :':'
-                  }
+        print('v_type %s' % v_type)
+        try:
+            type_dict={"<class 'list'>"  :','
+                      ,"<class 'str'>"   :'s'
+                      ,"<class 'dict'>"  :':'
+                      ,'datetime':'d'
+                      ,'int'     :'i'
+                      }
+        except:
+            print(v_type)
         return type_dict[v_type]
 
-    d=awsret2dict(aws_return)
-    print()
-    for k,v in d.items():
-        print('key_name %s, value type %s' % (k, type2delim(str(type(v)))))
+    def descend(input: Union[List,Dict,str,int]) -> None:
+        for k,v in d.items():
+            t=type2delim(str(type(v)))
+            print('key_name %s, value type %s' \
+                 % (k, type2delim(str(type(v)))))
+
+            #If we're dealing with a list or dict, we need to descend.
+            #  need a "descend iterator" that checks the type of the
+            #  container member and does key/value discrimination as required.
+            if t in [',',':']:
+                if t==',':
+                    for w in v:
+                        print(w)
+                else:
+                     pass
+
+    d: dict=awsret2dict(aws_return)
+    descend(d)
